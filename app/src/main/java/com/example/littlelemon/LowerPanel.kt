c997919e36e324db0b9e36152bc0e14e1d8090e7
Ab <46964181+abd-koumare@users.noreply.github.com>
2024-08-25 22:48:59 +0000

    initial

---
+++ app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -0,0 +1,113 @@
+package com.example.littlelemon
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.littlelemon.ui.theme.LittleLemonColor
+
+@Composable
+fun LowerPanel(navController: NavHostController, dishes: List<Dish> = listOf()) {
+    Column {
+        WeeklySpecialCard()
+        LazyColumn {
+            itemsIndexed(dishes) { _, dish ->
+                MenuDish(navController, dish)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeeklySpecialCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.weekly_special),
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MenuDish(navController: NavHostController? = null, dish: Dish) {
+    Card(onClick = {
+        Log.d("AAA", "Click ${dish.id}")
+        navController?.navigate(DishDetails.route + "/${dish.id}")
+    }) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = dish.name,
+                    style = MaterialTheme.typography.h2,
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(0.75f)
+                        .padding(top = 5.dp, bottom = 5.dp),
+                    text = dish.description,
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "${dish.price}",
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
+            Image (
+                modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+                painter = painterResource(id = dish.imageResource),
+                contentDescription = "Dish Image"
+            )
+
+        }
+
+    }
+    Divider(
+        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+        thickness = 1.dp,
+        color = LittleLemonColor.yellow
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LowerPanelPreview() {
+    LowerPanel(navController = NavHostController(LocalContext.current))
+}
